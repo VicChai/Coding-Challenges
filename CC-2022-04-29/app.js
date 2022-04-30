09e0ab4344fa4e05c861f9e45b216a149345d615
Victor Chai <vic.p.chai@gmail.com>
2022-04-29 23:02:00 -0400

    coding challenge and solution for 04-29-22 coding challenge

---
+++ CC-2022-04-29/app.js
@@ -0,0 +1,26 @@
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+//         '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+//         '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+//         '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+    // good luck
+    if(card[card.length-1] === '♣'){
+      return 'clubs'
+      }else if ( card[card.length-1] === '♠' ){
+      return 'spades'
+    }else if ( card[card.length-1] === '♦' ){
+      return 'diamonds'
+    }else if (card[card.length-1] === '♥' ){
+      return 'hearts'
+    }
+    
+  }
