27b5a058adedfa7afa33fd3a05655bc9488227c8
Victor Chai <vic.p.chai@gmail.com>
2022-05-28 20:22:24 -0400

    Coding challenge and solution for 05-28-22 coding challenge

---
+++ replaceAllVowelToExclamationMarkInTheSentence.js
@@ -0,0 +1,21 @@
+// Description:
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+
+function replace(s){
+    //coding and coding....
+    let newArr = []
+    for(let i = 0; i < s.length;i++){
+      if(s[i] === 'a' || s[i] === 'e' || s[i] === 'i' || s[i] === 'o' || s[i] === 'u' || s[i] === 'A' || s[i] === 'E' || s[i] === 'I' || s[i] === 'O' || s[i] === 'U') {
+        s[i] = newArr.push('!')
+      } else {
+        newArr.push(s[i])
+      }   
+    }
+    return newArr.join('')  
+  } 
