38da86cca0742f26697710628fe03006980d12e2
Victor Chai <vic.p.chai@gmail.com>
2022-05-30 17:23:55 -0400

    Coding challenge and solution for 05-30-22 coding challenge

---
+++ sumOfDifferencesInArray.js
@@ -0,0 +1,23 @@
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
+
+function sumOfDifferences(arr) {
+    let newArr = []
+     if(arr.length <= 1){
+       return 0
+     } else {
+         let sorted = arr.sort( (a,b)=> b-a ) 
+       for(let i = 0; i < arr.length - 1;i++){
+           newArr.push(arr[i] - arr[i+1])
+       }
+       return newArr.reduce( (acc,c) => acc + c, 0 )  
+     }                        
+   }
+   
