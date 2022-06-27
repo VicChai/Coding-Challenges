6fbffccfdf609ec4f0d41a9529f343b353ad9986
Victor Chai <vic.p.chai@gmail.com>
2022-06-27 12:18:31 -0400

    Coding Challenge and solution for 06-27-22 coding challenge

---
+++ Most digits.js
@@ -0,0 +1,9 @@
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+
+function findLongest(array){
+    let lengths = array.map(el=>el.toString().length)
+    return array.find(el=>el.toString().length === (Math.max(...lengths)))
+  } 
