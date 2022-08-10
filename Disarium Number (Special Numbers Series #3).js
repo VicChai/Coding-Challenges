50492f107ebb9dad26769481a114eca180025b99
Victor <victor0528@gmail.com>
2022-08-10 06:55:18 -0400

    Coding Challenge and solution for 08/10/22 coding challenge

---
+++ Disarium Number (Special Numbers Series #3).js
@@ -0,0 +1,24 @@
+// Definition
+// Disarium number is the number that The sum of its digits powered with their respective positions is equal to the number itself.
+
+// Task
+// Given a number, Find if it is Disarium or not .
+
+// Warm-up (Highly recommended)
+// Playing With Numbers Series
+// Notes
+// Number passed is always Positive .
+// Return the result as String
+// Input >> Output Examples
+// disariumNumber(89) ==> return "Disarium !!"
+// Explanation:
+// Since , 81 + 92 = 89 , thus output is "Disarium !!"
+// disariumNumber(564) ==> return "Not !!"
+// Explanation:
+// Since , 51 + 62 + 43 = 105 != 564 , thus output is "Not !!"
+
+function disariumNumber(n){
+    //your code here
+    let split = n.toString().split('').map((num,idx)=>num**(idx+1)).reduce((acc,c)=>acc+c)
+    return split === n ? "Disarium !!" : "Not !!"
+  }
