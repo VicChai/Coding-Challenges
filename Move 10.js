18aa1cb62d9076b47e2af0e827af2cd041fe2457
Victor Chai <vic.p.chai@gmail.com>
2022-09-25 10:21:24 -0400

    coding challenge and solution

---
+++ Move 10.js
@@ -0,0 +1,9 @@
+// Move every letter in the provided string forward 10 letters through the alphabet.
+
+// If it goes past 'z', start again at 'a'.
+
+// Input will be a string with length > 0.
+
+function moveTen(s){
+    return s.split('').map(el=> (el.charCodeAt() + 10) > 122 ? String.fromCharCode((el.charCodeAt() + 10) -26) : String.fromCharCode(el.charCodeAt() + 10)).join('')
+  }
