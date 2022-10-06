02bf42c168d3da102a4a6031fb425dc2bcd38af3
Victor Chai <vic.p.chai@gmail.com>
2022-10-06 11:58:43 -0400

    Coding challenge and solution

---
+++ CamelCase Method.js
@@ -0,0 +1,12 @@
+// Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+// For instance:
+
+// "hello case".camelCase() => HelloCase
+// "camel case word".camelCase() => CamelCaseWord
+// Don't forget to rate this kata! Thanks :)
+
+String.prototype.camelCase=function(){
+    //   your code here
+     return this.split(' ').map(el=>el.slice(0,1).toUpperCase() + el.slice(1)).join('')
+    }
