453d61a58627d0d88758322ad1d0eb6218170412
Victor Chai <vic.p.chai@gmail.com>
2022-12-07 12:40:55 -0500

    Coding challenge and solution

---
+++ Jewels and Stones.js
@@ -0,0 +1,19 @@
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+var numJewelsInStones = function (jewels, stones) {
+  let count = 0;
+  const map = {};
+
+  for (let jewel of jewels) {
+    map[jewel] = true;
+  }
+  for (let stone of stones) {
+    if (map[stone]) {
+      count++;
+    }
+  }
+
+  return count;
+};
