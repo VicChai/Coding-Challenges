6cfc1adc2bf9800e9edd6c4ced2a79cd958df1a4
Victor Chai <vic.p.chai@gmail.com>
2023-01-05 21:44:32 -0500

    Coding challenge and solution

--- Fibonacci Number.js
+++ Fibonacci Number.js
@@ -47,3 +47,16 @@ var fib = function (n) {
   }
   return result;
 };
+
+const memo = {};
+function fib(n) {
+  if (n <= 1) {
+    return n;
+  } else if (memo[n]) {
+    return memo[n];
+  } else {
+    let result = fib(n - 1) + fib(n - 2);
+    memo[n] = result;
+    return result;
+  }
+}
