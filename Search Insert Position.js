7b594e212aa1e99d219377b22522d023a49b3c8f
Victor Chai <vic.p.chai@gmail.com>
2023-01-22 21:21:00 -0500

    Coding challenge and solution

--- Search Insert Position.js
+++ Search Insert Position.js
@@ -15,3 +15,20 @@ var searchInsert = function (nums, target) {
   }
   return nums.length;
 };
+
+var searchInsert = function(nums, target) {
+  let left = 0
+  let right = nums.length -1
+  let idx = -1
+
+  while(left <= right){
+      let mid = left + Math.floor((right - left)/2)
+      if(nums[mid] === target) return mid
+      if(nums[mid] < target){
+          left = mid + 1
+          idx = mid
+      }else {
+          right = mid -1
+      }
+  }
+  return idx+1
