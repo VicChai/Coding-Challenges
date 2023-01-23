1f497a8718d1e3f0613986f418701138ba8ff7c0
Victor Chai <vic.p.chai@gmail.com>
2023-01-23 15:03:56 -0500

    Coding challenge and solution

--- Remove duplicates from list.js
+++ Remove duplicates from list.js
@@ -3,5 +3,17 @@
 // The order of the sequence has to stay the same.
 
 function distinct(a) {
-    return [...new Set(a)] 
+  return [...new Set(a)];
+}
+
+function removeDuplicates(arr) {
+  // WRITE YOUR BRILLIANT CODE HERE
+  let slow = 0;
+  for (let fast = 0; fast < arr.length; fast++) {
+    if (arr[fast] !== arr[slow]) {
+      slow++;
+      arr[slow] = arr[fast];
+    }
   }
+  return slow + 1;
+}
